6b613cd03ab3526001cee21a1ed75fe24b9811d8
Tytan Telford <tytantelford@dpadmins-MBP.lan>
2025-03-28 12:05:55 -0600

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -26,14 +26,25 @@ function Main() {
   return (
     <div className="main-container">
       <img className="main-img" src="/public/r2d2.jpg" />
-      <span className="header">HEADER</span>
-      <span className="subheader">SUBHEADER</span>
-      <span className="main-content">MAINCONTENT</span>
+      <span className="header">R2D2</span>
+      <span className="subheader">
+        R2-D2 is a brave and loyal astromech droid, known for his
+        resourcefulness and iconic beeps.
+      </span>
+      <span className="main-content">
+        R2-D2 is a small but incredibly skilled droid who has saved the galaxy
+        time and time again with his intelligence and bravery. With his trusty
+        beeps and whistles, he’s become an iconic symbol of loyalty and
+        resourcefulness in the Star Wars universe.
+      </span>
       <div className="grid-container">
         <div className="grid">grid</div>
         <img className="grid" src="/public/r2.jpg" alt="" />
         <img className="grid" src="/public/r2d2-grid.jpeg" alt="" />
-        <div className="grid">grid</div>
+        <div className="grid">
+          R2-D2 often hides in tall grass, using his small size to stay
+          unnoticed, quietly observing and waiting for action.
+        </div>
       </div>
       <div className="main-content-bottom">MAIN CONTENT</div>
       <button className="button-main"></button>
